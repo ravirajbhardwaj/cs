28e340a8faf0c597193dd4f8e14d2826e0fefd1e
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2026-01-16 00:54:20 +0530

    feat: add hof & cb function

--- Programming/_functions/callback.js
+++ Programming/_functions/callback.js
@@ -1,10 +1,21 @@
-function greet(name, callback) {
-  console.log(`Hi, ${name}`);
-  callback();
+// High Order Functions (HOF) are functions that meet at least one condition:
+// 1 It accepts one or more functions as arguments, OR
+// 2 It returns a function as its result
+
+// Callback Functions
+// A callback function is a function passed into another function as an argument,
+// which is then invoked inside the outer function to complete some kind of routine or action.
+
+//                                 callback
+function calculate(numOne, numTwo, operation) {
+  return operation(numOne, numTwo);
 }
 
-function sayBye() {
-  console.log("GoodBye!");
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
 
-greet("Ravi", sayBye);
+console.log(calculate(5, 3, add));
+console.log(calculate(5, 3, subtract));
+
+// calculate function is a HOF because it takes another function (operation) as an argument.
+// The add and subtract functions are callback functions passed to calculate.
